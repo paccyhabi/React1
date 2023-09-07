5bda5896bc9df05d0ac0a1a0c542b83f2d8148de
paccyhabi <140205164+paccyhabi@users.noreply.github.com>
2023-09-07 15:11:42 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import ListGroup from "./components/ListGroup";
+function App(){
+  let items = ["Ney York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem =(item: string) => {
+    console.log(item);
+  }
+  return <div><ListGroup items = {items} heading="Cities" onSelectItem={handleSelectItem}/></div>;
+}
+export default App;
